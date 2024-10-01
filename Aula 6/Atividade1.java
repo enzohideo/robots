c8ed4cfb7752653927210b6367e7acf120df3a5c
pato <enzohideo@users.noreply.github.com>
2024-10-01 17:16:53 -0300

    a6: Add Atividade1

---
+++ Aula 6/Atividade1.java
@@ -0,0 +1,46 @@
+/*
+ *
+ * Aula 6 - Programa 1
+ *
+*/
+
+import lejos.nxt.LCD;
+import lejos.nxt.Button;
+import lejos.nxt.Sound;
+import lejos.util.Delay;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.*;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
+
+public class Atividade1 {
+  public static void main(String[] args) {
+    Button.waitForAnyPress();
+
+    LCD.clear();
+    LCD.drawString("Sensor de Toque", 0, 0);
+
+    TouchSensor toque = new TouchSensor(SensorPort.S3);
+    UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
+    DifferentialPilot pilot = new DifferentialPilot(2.205 * 2.56, 4.527 * 2.56,
+                               Motor.A, Motor.B);
+
+    pilot.forward();
+    while (sonic.getDistance() > 30) {}
+    pilot.stop();
+
+    pilot.arc(30, -90, true);
+
+    while (!toque.isPressed() && pilot.isMoving()) {}
+    if (toque.isPressed()) {
+      LCD.drawString("bateu na parede", 0, 1);
+      pilot.stop();
+      Sound.playTone(500, 2000);
+    }
+    Delay.msDelay(1000);
+
+    pilot.arc(-30, -90, false);
+    pilot.travel(30, false);
+  }
+}
