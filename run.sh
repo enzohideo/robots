5ef9da46c77f677c07ad6c33539a4b225e8dea8b
pato <enzohideo@users.noreply.github.com>
2024-10-01 16:48:35 -0300

    scripts: Add run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+error() {
+  echo -e "\e[31mFAILED\e[0m"
+}
+
+pc() {
+  program="$1"
+  basename="${program%%.*}"
+
+  nxjpcc "$program" \
+   && nxjpc "$basename" \
+   || error
+}
+
+up() {
+  program="$1"
+  basename="${program%%.*}"
+
+  nxjc "$program" \
+    && nxj -o "${basename}.nxj" "${basename}" ${@:2} \
+    || error
+}
