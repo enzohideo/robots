42ebfaab8206e9fca19e0e11f81afe4f56e09438
pato <enzohideo@users.noreply.github.com>
2024-10-22 14:53:35 -0300

    a8/checkline: Add measurements

--- Aula 8/CheckLine.java
+++ Aula 8/CheckLine.java
@@ -4,6 +4,10 @@ import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
 import lejos.util.Delay;
 
+// black  40~41
+// middle 47-52
+// white  61~62
+
 public class CheckLine{
   public static void main(String [] args){
     LightSensor light = new LightSensor(SensorPort.S4);
