7c45747e1dd127b4ce0999d8d60c156120ac0934
duck <noreply@github.com>
2024-11-19 16:09:42 -0300

    project: Add Dijkstra

--- project/Project.java
+++ project/Project.java
@@ -72,111 +72,289 @@ class Claw {
 
 }
 
-// class GeneratePath implements IState {
-//   
-//   private boolean[][] adjacency_matrix;
-//
-//   public GeneratePath() {
-//     this.adjacency_matrix = 
-//   }
-//
-//   public boolean[][] WaipointsGraph() {
-//     Waipoint point0 = new Waipoint(15.0, 15.0);
-//     Waipoint point1 = new Waipoint(15.0, 30.0);
-//     Waipoint point2 = new Waipoint(15.0, 45.0);
-//     Waipoint point3 = new Waipoint(15.0, 60.0);
-//     Waipoint point4 = new Waipoint(15.0, 75.0);
-//     Waipoint point5 = new Waipoint(30.0, 15.0);
-//     Waipoint point6 = new Waipoint(30.0, 30.0);
-//     Waipoint point7 = new Waipoint(30.0, 45.0);
-//     Waipoint point8 = new Waipoint(30.0, 60.0);
-//     Waipoint point9 = new Waipoint(30.0, 75.0);
-//     Waipoint point10 = new Waipoint(45.0, 15.0);
-//     Waipoint point11 = new Waipoint(45.0, 30.0);
-//     Waipoint point12 = new Waipoint(45.0, 45.0);
-//     Waipoint point13 = new Waipoint(45.0, 60.0);
-//     Waipoint point14 = new Waipoint(45.0, 75.0);
-//     Waipoint point15 = new Waipoint(60.0, 15.0);
-//     Waipoint point16 = new Waipoint(60.0, 30.0);
-//     Waipoint point17 = new Waipoint(60.0, 45.0);
-//     Waipoint point18 = new Waipoint(60.0, 60.0);
-//     Waipoint point19 = new Waipoint(60.0, 75.0);
-//     Waipoint point20 = new Waipoint(75.0, 15.0);
-//     Waipoint point21 = new Waipoint(75.0, 30.0);
-//     Waipoint point22 = new Waipoint(75.0, 45.0);
-//     Waipoint point23 = new Waipoint(75.0, 60.0);
-//     Waipoint point24 = new Waipoint(75.0, 75.0);
-//
-//     boolean[][] matrix = new boolean[25][25];
-//     for(int i = 0; i < 25; i++) {
-//       for(int j = 0; j < 25; j++) {
-//         matrix[i][j] = false;
-//       }
-//     }
-//     matrix[0][1] = true;
-//     matrix[1][0] = true;
-//     matrix[1][2] = true;
-//     matrix[1][6] = true;
-//     matrix[2][1] = true;
-//     matrix[2][3] = true;
-//     matrix[3][2] = true;
-//     matrix[3][4] = true;
-//     matrix[3][8] = true;
-//     matrix[4][3] = true;
-//     //matrix[4][9] = true;
-//     //matrix[5][6] = true; 5 = School
-//     //matrix[5][10] = true;
-//     matrix[6][1] = true;
-//     //matrix[6][5] = true;
-//     //matrix[6][7] = true;
-//     matrix[6][11] = true;
-//     //matrix[7][6] = true; 7 = City Hall
-//     //matrix[7][8] = true;
-//     matrix[8][3] = true;
-//     matrix[8][7] = true;
-//     matrix[8][9] = true;
-//     matrix[8][13] = true;
-//     //matrix[9][4] = true; 9 = Library
-//     //matrix[9][8] = true;
-//     //matrix[10][5] = true;
-//     matrix[10][11] = true;
-//     matrix[11][6] = true;
-//     matrix[11][10] = true;
-//     matrix[11][12] = true;
-//     matrix[11][16] = true;
-//     matrix[12][11] = true;
-//     matrix[12][13] = true;
-//     matrix[13][8] = true;
-//     matrix[13][12] = true;
-//     matrix[13][14] = true;
-//     matrix[13][18] = true;
-//     matrix[14][13] = true;
-//     // 15 = Bakery
-//     matrix[16][11] = true;
-//     matrix[16][21] = true;
-//     // 17 = Drugstore 
-//     matrix[18][13] = true;
-//     matrix[18][23] = true;
-//     // 19 = Museum
-//     matrix[20][21] = true;
-//     matrix[21][16] = true;
-//     matrix[21][20] = true;
-//     matrix[21][22] = true;
-//     matrix[22][21] = true;
-//     matrix[22][23] = true;
-//     matrix[23][18] = true;
-//     matrix[23][22] = true;
-//     matrix[23][24] = true;
-//     matrix[24][23U = true;
-//     return matrix;
-//   }
-//
-//   public void run() {
-//
-//   }
-// }
-//
-//
+
+
+class GeneratePath implements IState {
+
+  //private boolean[][] adjacency_matrix;
+
+
+  // class Node {
+  //   public static enum Wall {
+  //     RIGHT,
+  //     UP,
+  //     LEFT,
+  //     DOWN
+  //   }
+
+  //   static int wall_to_state(Wall wall) {
+  //     switch(wall) {
+  //       case RIGHT:
+  //         return 1;
+  //       case UP:
+  //         return 1 << 2;
+  //       case LEFT:
+  //         return 1 << 4;
+  //       case DOWN:
+  //         return 1 << 6;
+  //     }
+  //     return 0;
+  //   }
+
+  //   static byte create_state(
+  //     boolean right,
+  //     boolean up,
+  //     boolean left,
+  //     boolean down
+  //   ) {
+  //     byte state = 0;
+
+  //     if (right) state += wall_to_state(Wall.RIGHT);
+  //     if (up) state += wall_to_state(Wall.UP);
+  //     if (left) state += wall_to_state(Wall.LEFT);
+  //     if (down) state += wall_to_state(Wall.DOWN);
+
+  //     return state;
+  //   }
+
+  //   public boolean is_closed() {
+
+  //   }
+
+  //   private byte state;
+
+  //   public Node(
+  //     boolean right,
+  //     boolean up,
+  //     boolean left,
+  //     boolean down
+  //   ) {
+  //     state = create_state(right, up, left, down);
+  //   }
+  // }
+
+  private boolean[][] adjacency_matrix;
+  private Waypoint[] nodes;
+  private List<Waypoint> path;
+
+  public GeneratePath() {
+    this.nodes = init_list_of_nodes();
+    this.adjacency_matrix = init_adjacency_matrix();
+  }
+
+  public static Waypoint[] init_list_of_nodes() {
+    Waypoint[] points = new Waypoint[25];
+
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        points[i*5 + j] = new Waypoint((i + 1) * 15.0, (j + 1) * 15.0);
+      }
+    }
+    return points;
+  }
+
+  public static boolean[][] init_adjacency_matrix() {
+
+    boolean[][] matrix = new boolean[25][25];
+    for(int i = 0; i < 25; i++) {
+      for(int j = 0; j < 25; j++) {
+        matrix[i][j] = false;
+      }
+    }
+    matrix[0][1] = true;
+    matrix[1][0] = true;
+    matrix[1][2] = true;
+    matrix[1][6] = true;
+    matrix[2][1] = true;
+    matrix[2][3] = true;
+    matrix[3][2] = true;
+    matrix[3][4] = true;
+    matrix[3][8] = true;
+    matrix[4][3] = true;
+    // 5 = School
+    matrix[6][1] = true;
+    matrix[6][11] = true;
+    // 7 = City Hall
+    matrix[8][3] = true;
+    matrix[8][13] = true;
+    // 9 = Library
+    matrix[10][11] = true;
+    matrix[11][6] = true;
+    matrix[11][10] = true;
+    matrix[11][12] = true;
+    matrix[11][16] = true;
+    matrix[12][11] = true;
+    matrix[12][13] = true;
+    matrix[13][8] = true;
+    matrix[13][12] = true;
+    matrix[13][14] = true;
+    matrix[13][18] = true;
+    matrix[14][13] = true;
+    // 15 = Bakery
+    matrix[16][11] = true;
+    matrix[16][21] = true;
+    // 17 = Drugstore
+    matrix[18][13] = true;
+    matrix[18][23] = true;
+    // 19 = Museum
+    matrix[20][21] = true;
+    matrix[21][16] = true;
+    matrix[21][20] = true;
+    matrix[21][22] = true;
+    matrix[22][21] = true;
+    matrix[22][23] = true;
+    matrix[23][18] = true;
+    matrix[23][22] = true;
+    matrix[23][24] = true;
+    matrix[24][23] = true;
+    return matrix;
+  }
+
+  public void close_museum() {
+    this.adjacency_matrix[14][19] = false;
+    this.adjacency_matrix[18][19] = false;
+  }
+
+  public void open_museum() {
+    this.adjacency_matrix[14][19] = true;
+    this.adjacency_matrix[18][19] = true;
+  }
+
+  public void close_drugstore() {
+    this.adjacency_matrix[12][17] = false;
+    this.adjacency_matrix[22][17] = false;
+  }
+
+  public void open_drugstore() {
+    this.adjacency_matrix[12][17] = true;
+    this.adjacency_matrix[22][17] = true;
+  }
+
+  public void close_bakery() {
+    this.adjacency_matrix[16][15] = false;
+    this.adjacency_matrix[20][15] = false;
+  }
+
+  public void open_bakery() {
+    this.adjacency_matrix[16][15] = true;
+    this.adjacency_matrix[20][15] = true;
+  }
+
+  public void close_school() {
+    this.adjacency_matrix[6][5] = false;
+    this.adjacency_matrix[10][5] = false;
+  }
+
+  public void open_school() {
+    this.adjacency_matrix[6][5] = true;
+    this.adjacency_matrix[10][5] = true;
+  }
+
+  public void close_cityhall() {
+    this.adjacency_matrix[8][7] = false;
+    this.adjacency_matrix[6][7] = false;
+  }
+
+  public void open_cityhall() {
+    this.adjacency_matrix[8][7] = true;
+    this.adjacency_matrix[6][7] = true;
+  }
+
+  public void close_library() {
+    this.adjacency_matrix[4][9] = false;
+    this.adjacency_matrix[8][9] = false;
+  }
+
+  public void open_library() {
+    this.adjacency_matrix[4][9] = true;
+    this.adjacency_matrix[8][9] = true;
+  }
+
+  public static List<Integer> dijkstra(boolean[][] adjMatrix, int start, int end) {
+    int n = adjMatrix.length;
+
+    // Distâncias mínimas inicializadas com infinito
+    int[] distances = new int[n];
+    Arrays.fill(distances, Integer.MAX_VALUE);
+    distances[start] = 0;
+
+    // Array para rastrear os vértices anteriores no menor caminho
+    int[] previous = new int[n];
+    Arrays.fill(previous, -1);
+
+    // Conjunto de vértices visitados
+    boolean[] visited = new boolean[n];
+
+
+    List<Integer> queue = new ArrayList<>();
+    queue.add(start);
+
+    while (!queue.isEmpty()) {
+        // Encontrar o vértice com a menor distância na lista
+        int current = -1;
+        int minDistance = Integer.MAX_VALUE;
+        for (int vertex : queue) {
+            if (distances[vertex] < minDistance) {
+                minDistance = distances[vertex];
+                current = vertex;
+            }
+        }
+
+        // Remover o vértice atual da lista
+        queue.remove((Integer) current);
+
+        // Se o vértice já foi visitado, ignorá-lo
+        if (visited[current]) continue;
+        visited[current] = true;
+
+        // Verificar todos os vizinhos
+        for (int neighbor = 0; neighbor < n; neighbor++) {
+            if (adjMatrix[current][neighbor] && !visited[neighbor]) {
+                int newDist = distances[current] + 1; // Peso uniforme (1)
+                if (newDist < distances[neighbor]) {
+                    distances[neighbor] = newDist;
+                    previous[neighbor] = current;
+                    if (!queue.contains(neighbor)) {
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
+    // Reconstruir o caminho mais curto
+    List<Integer> path = new ArrayList<>();
+    for (int at = end; at != -1; at = previous[at]) {
+        path.add(at);
+    }
+
+    // Reverter a lista manualmente
+    List<Integer> reversedPath = new ArrayList<>();
+    for (int i = path.size() - 1; i >= 0; i--) {
+        reversedPath.add(path.get(i));
+    }
+
+    // Se o caminho não começa com o vértice inicial, significa que não há caminho
+    if (reversedPath.isEmpty() || reversedPath.get(0) != start) {
+        return new ArrayList<>(); // Retorna uma lista vazia
+    }
+
+    return reversedPath;
+  }
+
+  public void WaypointsPath(List<Integer> nodes) {
+    List<Waypoint> path = new ArrayList<Waypoint>();
+    for (int i = 0; i < path.size(); i++) {
+      path.add(this.nodes[nodes.get(i)]);
+    }
+    this.path = path;
+  }
+
+  public void run() {
+
+  }
+}
+
 
 class ColorPID {
   public static float uLine = 0;
