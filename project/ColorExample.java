228fead6ff4fd6bb5241c4bcb6b0dd490e65e94b
enzohideo <enzohideo@users.noreply.github.com>
2024-11-21 13:44:48 -0300

    ColorExample: Set white floodlight

--- project/ColorExample.java
+++ project/ColorExample.java
@@ -9,15 +9,19 @@ public class ColorExample {
 
   public static void main(String[] args) {
     colorSensor = new ColorSensor(SensorPort.S1);
+    colorSensor.setFloodlight(Color.WHITE);
 
     LCD.drawString("Color Sensor", 0, 0);
     Button.waitForAnyPress();
 
     while(true) {
       Color color = colorSensor.getColor();
+
       LCD.drawString("red: " + Integer.toString(color.getRed()), 0, 1);
       LCD.drawString("green: " + Integer.toString(color.getGreen()), 0, 2);
       LCD.drawString("blue: " + Integer.toString(color.getBlue()), 0, 3);
+      LCD.drawString("light value: " + Integer.toString(colorSensor.getLightValue()), 0, 4);
+
       try {
         Thread.sleep(1000);
       } catch(InterruptedException error) {
