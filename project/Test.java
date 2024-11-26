6c175750252a5abf2bbe22272b0c5cc3eab6193f
enzohideo <enzohideo@users.noreply.github.com>
2024-11-26 02:59:06 -0300

    project: Add basic Test class

---
+++ project/Test.java
@@ -0,0 +1,26 @@
+class Test {
+  static final String CLEAR = "\u001B[0m";
+  static final String GREEN = "\u001B[32m";
+  static final String RED = "\u001B[31m";
+
+  static public boolean check(String name, boolean assertion) {
+    if (!assertion)
+      failed(name);
+    return assertion;
+  }
+
+  static public void passed(String testName) {
+    System.out.printf("%sPASSED:%s %s\n", GREEN, CLEAR, testName);
+  }
+
+  static public void failed(String testName) {
+    System.out.printf("%sFAILED:%s %s\n", RED, CLEAR, testName);
+  }
+
+  public static void main(String[] args) {
+    passed("test passed");
+    failed("test failed");
+    check("hello", true);
+    check("world", false);
+  }
+}
