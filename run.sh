2f5ab8aa9820093e46879665d290542d84ec16c1
enzohideo <enzohideo@users.noreply.github.com>
2024-11-26 01:38:02 -0300

    nix: Update lockfile to allow multi-file compilation

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ pc() {
   program="$1"
   basename="${program%%.*}"
 
-  nxjpcc "$program" \
+  nxjpcc "$program" ${@:2} \
    && nxjpc "$basename" \
    || error
 }
@@ -17,8 +17,8 @@ up() {
   program="$1"
   basename="${program%%.*}"
 
-  nxjc "$program" \
-    && nxj -o "${basename}.nxj" "${basename}" ${@:2} \
+  nxjc "$program" ${@:2} \
+    && nxj -o "${basename}.nxj" "${basename}" \
     || error
 }
 
